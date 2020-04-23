397e5e370805e278884c7918cdaab6a71d04ebc0
Sebastian Lopez H <lopez.sebastian@correounivalle.edu.co>
2020-04-22 21:24:19 -0500

    Updates

---
+++ precache-manifest.8802322602276a72a36495c9cf0f9c9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0229b408f4499674fc97dbe2da995747",
+    "url": "/react-weather-app/index.html"
+  },
+  {
+    "revision": "1b7b2fd0fdcd61a1320f",
+    "url": "/react-weather-app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "fc392e4121dc0f79328d",
+    "url": "/react-weather-app/static/css/main.774c232e.chunk.css"
+  },
+  {
+    "revision": "1b7b2fd0fdcd61a1320f",
+    "url": "/react-weather-app/static/js/2.95af09aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-weather-app/static/js/2.95af09aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fc392e4121dc0f79328d",
+    "url": "/react-weather-app/static/js/main.943a742f.chunk.js"
+  },
+  {
+    "revision": "4e02c4ee54615593cabf",
+    "url": "/react-weather-app/static/js/runtime-main.7a57be8c.js"
+  },
+  {
+    "revision": "b6796c732547b6b7e19cdc8e4ad58356",
+    "url": "/react-weather-app/static/media/bg.b6796c73.jpg"
+  }
+]);
